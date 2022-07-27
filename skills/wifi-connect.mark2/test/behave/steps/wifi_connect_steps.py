31610cda68f46fd7770dc92be1f61c1c7ce61969
Michael Hansen <michael.hansen@mycroft.ai>
2022-07-27 20:35:13 +0100

    Remove unnecessary skills (moved to enclosure)

--- skills/wifi-connect.mark2/test/behave/steps/wifi_connect_steps.py
+++
@@ -1,7 +0,0 @@
-from behave import when
-from mycroft.messagebus import Message
-
-
-@when("no network is detected")
-def emit_message(context):
-    context.bus.emit(Message("hardware.network-not-detected"))
