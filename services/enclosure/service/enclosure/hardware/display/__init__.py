476c6496e13df1ae263814cc2d8e1a7e75935411
Michael Hansen <michael.hansen@mycroft.ai>
2022-07-28 21:48:25 +0100

    Remove unnecessary files

--- services/enclosure/service/enclosure/hardware/display/__init__.py
+++
@@ -1,1 +0,0 @@
-from .gui.namespace import NamespaceManager
--- services/enclosure/service/enclosure/mark2/activities/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from .access_point import AccessPointActivity
-from .internet_connect import InternetConnectActivity
-from .network_connect import NetworkConnectActivity
-from .system_clock_sync import SystemClockSyncActivity
